0c83f93d44ab99ec3df05fd92969dbc18b196458
Santiago Sainz Fernández <evelpialight@gmail.com>
2014-09-24 23:18:19 +0100

    Little changes

--- configure.rb
+++ configure.rb
@@ -1,7 +1,7 @@
 def my_twitter_client
 	Twitter::REST::Client.new do |config|
-		config.consumer_key	= 'SI3mmPIrHtTGYUxzkiIx3sXbO'
-		config.consumer_secret	= 'RHIkTuCeCOLLgcwD7qPPR7RporwVJ8l2BTJmLv25JuQJrXDCRZ'
+		config.consumer_key	= 'GiLdvV7GmYXQZgr8fNPsipOr8'
+		config.consumer_secret	= 'R4isKoUf2ZwLvgN1RuqBs08Z5KuE1zJOdmrTAfMHzVc4EwZ2td'
 		config.access_token	= '226946128-RQYcLrkOhqDzUFTt09cwHCh1XwtJsQGltNa57EGn'
 		config.access_token_secret = '60HLVvqAaPnAZB4amydLncxZiMvLfVvGBkhcTxdsdZ8WX'
 	end
--- twitter.rb
+++ twitter.rb
@@ -5,21 +5,31 @@ require 'sinatra'
 require './configure'
 
 get '/' do
-	@todo_tweet = []
+	#@todo_tweet = []
+	@seguidores = []
 	@name = ''
 	@number = 0
 	erb :twitter
 end
 
 post '/' do
-	@todo_tweet = []
+	#@todo_tweet = []
+	@seguidores = []
 	client = my_twitter_client()
 	@name = params[:firstname] || ''
 	@number = params[:n].to_i
 	@number = 1 if @number <= 0
-	if client.user? @name
-		ultimos_t = client.user_timeline(@name,{:count=>@number.to_i})
-		@todo_tweet = (@todo_tweet != '') ? ultimos_t.map{ |i| i.text} : ''
+	client.user(@name)
+	if client.user?
+		populares = client.friends.take(@number)
+		@seguidores = populares
 	end
+	#ni = @number
+	#nombrecito = @name
+	#array = client.friend_ids(nombrecito).attrs[:ids].take(ni)
+	#if client.user?
+		#ultimos_t = client.user_timeline(@name,{:count=>@number.to_i})
+		#@todo_tweet =(@todo_tweet != '') ? ultimos_t.map{ |i| i.text} : ''
+	#end
 	erb :twitter
 end
