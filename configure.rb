614ad19375c43c42b3243b97758607aa8fd0dc17
Santiago Sainz Fernández <evelpialight@gmail.com>
2014-09-25 20:23:16 +0100

    Code Update

--- configure.rb
+++ configure.rb
@@ -1,8 +1,8 @@
 def my_twitter_client
 	Twitter::REST::Client.new do |config|
-		config.consumer_key	= 'g1i3utmkFireb2nXkqNFEVYU9'
-		config.consumer_secret	= 'eiy8uZAM0wZdju9I6oj97Y7fOUJJgKmPaw7p5rQEkUS72o5KZj'
-		config.access_token	= '226946128-7K2FZPD9n9RUbw5ZZciRtWn3fE7yYRXnpqFTRBcX'
-		config.access_token_secret = 'hP6HIC8LyCWYzbIsKpUn7rgBLo1MzUfJ7CU2PmDQc3ZA9'
+		config.consumer_key	= '*************'
+		config.consumer_secret	= '************'
+		config.access_token	= '*************'
+		config.access_token_secret = '*************'
 	end
 end
